f65e28f88fc7a1d3ccc87ce6af6218225ffd7304
stuwilmur <stuwilmur@users.noreply.github.com>
2021-11-26 12:38:08 +0000

    Complete a code tidy and commenting.

--- gore/gore2.py
+++ gore/gore2.py
@@ -12,6 +12,7 @@ import math as mt
 import matplotlib.pyplot as plt
 import cv2
 
+
 """
 basic trigonometric functions
 """
@@ -19,12 +20,14 @@ cot = lambda z : 1 / mt.tan(z)
 cosec = lambda z : 1 / mt.sin(z)
 sec = lambda z : 1 / mt.cos(z)
 
+
 """
 constants: projection options
 """
 SINUSOIDAL = 0
 CASSINI = 1
 ORTHOGRAPHIC = 2
+
 
 def fig(img):
     """
@@ -38,6 +41,7 @@ def fig(img):
     plt.imshow(img)
     plt.show()
     
+    
 def image_from_path(path):
     """
     image_from_path:    open an image as a numpy ndarray
@@ -46,10 +50,15 @@ def image_from_path(path):
     
     returns             PIL.Image
     """
+    
+    # read the image
     im = cv2.imread(path)
+    
+    # swap the red and blue channels
     imRgb = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
     
     return imRgb
+
 
 def deg2rad(x):
     """
@@ -59,7 +68,9 @@ def deg2rad(x):
     
     returns:    angle (radians)
     """
+    
     return x / 180 * np.pi
+
 
 def rad2deg(x):
     """
@@ -69,7 +80,9 @@ def rad2deg(x):
     
     returns:    angle (degrees)
     """
+    
     return x / np.pi * 180
+
 
 def nd2im(arr):
     """
@@ -79,7 +92,9 @@ def nd2im(arr):
     
     returns     image (PIL.Image)
     """
+    
     return Image.fromarray(arr, mode="RGBA")
+
 
 def make_equatorial (im,
                     num_gores, 
@@ -106,13 +121,24 @@ def make_equatorial (im,
     """
     
     h, w = im.shape[:2]
+    
+    # create separate arrays of phi/lambda polar coordinates spanning the extent
     phi_vector, lam_vector = np.linspace(phi_min, phi_max, h, dtype = np.float32), np.linspace(lam_min, lam_max, w, dtype = np.float32)
     lam_dst, phi_dst = np.meshgrid(lam_vector, phi_vector)
+    
+    # create an index vector, used to find meridians
     indx = np.arange(w, dtype = np.float32)
+    
+    # calculate angular size of a gore
     gore_width = (lam_max - lam_min) / num_gores
+    
+    # calculate the meridians and produce a coordinate array
     lam00 = (indx // (w / num_gores)) * gore_width + gore_width / 2 + lam_min
     lam0 = np.tile(lam00, (h,1))
     
+    # do the appropriate projection (note, we use the reverse projection, i.e. 
+    # for every coordinate in the destination image, calculate its corresponding
+    # position in the source image.
     if projection == SINUSOIDAL:
         lam_src = ((lam_dst - lam0) / np.cos(phi_dst)) + lam0
         phi_src = phi_dst
@@ -130,14 +156,21 @@ def make_equatorial (im,
         lam_src = lam0 + np.arctan2(np.tan(lam_dst-lam0),np.cos(phi_dst))
         phi_src = np.arcsin(np.sin(phi_dst) * np.cos(lam_dst-lam0))
     
+    # limit each projection to within its own gore
     lam_src = lam_src + np.array(np.greater(lam_src, lam0 + gore_width / 2) * 1000, dtype = np.float32)
     lam_src = lam_src + np.array(np.less(lam_src, lam0 - gore_width / 2) * -1000, dtype = np.float32)
     
+    # convert polar coordinates back to source pixels
     y_src = (phi_src - phi_min) * h / (phi_max - phi_min)
     x_src = (lam_src - lam_min) * w / (lam_max - lam_min)
+    
+    # handle transparency
     transparent_img = np.zeros((h, w, 4), dtype=np.uint8)
     bgra = cv2.cvtColor(im, cv2.COLOR_RGB2RGBA)
+    
+    # perform the projection
     dst = cv2.remap(bgra, x_src, y_src, cv2.INTER_LINEAR, transparent_img)
+    
     return(dst)
     
 
@@ -152,21 +185,29 @@ def make_polar (im,
     # demand that the pole is included if the gores are to be stitched at the pole
     phi_min = -mt.pi / 2
     
+    # calculate basic quantities
     ht, wd = im.shape[:2]
-    
     ang_wd = lam_max - lam_min    
     rads_per_meridian = ang_wd / num_gores
     degs_per_meridian = rad2deg(rads_per_meridian)
-    
     gore_wd = wd // num_gores
+    
+    # create new image for the output
     pole_stitched = Image.new(mode = "RGBA", size = (2 * ht, 2 * ht))
     
-    equator_stitched_arr = make_equatorial(im = im, num_gores = num_gores,
-                                           phi_min = phi_min, phi_max = phi_max, lam_min = lam_min,
-                                           lam_max = lam_max, projection = projection)
+    # perform the goring
+    equator_stitched_arr = make_equatorial(im = im, 
+                                           num_gores = num_gores,
+                                           phi_min = phi_min, 
+                                           phi_max = phi_max, 
+                                           lam_min = lam_min,
+                                           lam_max = lam_max, 
+                                           projection = projection)
     
+    # convert to PIL.Image
     equator_stitched = nd2im(equator_stitched_arr)
     
+    # crop each gore, rotate it and repaste it in the rotary pattern
     for i in range(num_gores):
         strip = equator_stitched.crop((i * gore_wd, 0, (i+1) * gore_wd, ht))
         ht3 = int(ht * 1.5)
@@ -180,16 +221,17 @@ def make_polar (im,
         pos_x, pos_y = int(pos_x), int(pos_y)
         
         # using the gore as a mask means that the pasted bakground is transparent
-        pole_stitched.paste(gore, (pos_x, pos_y), mask=gore)
+        pole_stitched.paste(gore, (pos_x, pos_y), mask = gore)
         
     return pole_stitched
     
+
 def swap (im, 
          phi_extent = mt.pi / 2, 
          lam_extent = mt.pi):
     """
-    swap    takes an equirectangular projection of a certain angular
-            extent and rotates it about the y-axis, so the poles lie 
+    swap    takes an equirectangular (plate-caree) projection of a certain 
+            angular extent and rotates it about the y-axis, so the poles lie 
             at the equator and the equator becomes a meridian.
             
     im:         input image (ndarray)
@@ -198,24 +240,37 @@ def swap (im,
     
     returns:    output image (ndarray)
     """
+    
+    # calculate basic quantities
     h, w = im.shape[:2]
+    
+    # calculate the angular extents
     phi_dst_min, phi_dst_max, lam_dst_min, lam_dst_max = -np.pi / 2, np.pi / 2, 0, 2 * np.pi
     phi_src_min, phi_src_max, lam_src_min, lam_src_max = -phi_extent, phi_extent, -lam_extent, lam_extent
+    
+    # create arrays of polar coordinates spanning the extent
     phi_vector, lam_vector = np.linspace(phi_dst_min, phi_dst_max, h, dtype = np.float32), np.linspace(lam_dst_min, lam_dst_max, w, dtype = np.float32)
     lam_dst, phi_dst = np.meshgrid(lam_vector, phi_vector)
+    
+    # prepare the rotation: this is a pi/2 rotation about the y-axis
     phi_src = np.arcsin(np.cos(lam_dst) * np.cos(phi_dst))
     lam_src = np.arctan2(np.sin(lam_dst) * np.cos(phi_dst),-np.sin(phi_dst))
     y_src = (phi_src - phi_src_min) * h / (phi_src_max - phi_src_min)
     x_src = (lam_src - lam_src_min) * w / (lam_src_max - lam_src_min)
+    
+    # perform the remap
     dst = cv2.remap(im, x_src, y_src, cv2.INTER_LINEAR, cv2.BORDER_TRANSPARENT)
+    
+    
     return dst
+
 
 def equi(im, 
          alpha_max, 
          focal_length = 24):
     """
-    equi         takes a fundus image and computes its equirectangular projection
-                 assuming a simple spherical eye model
+    equi         takes a fundus image and computes its equirectangular (plate caree) 
+                 projection assuming a simple spherical eye model
 
     im           input image (ndarray)
             
@@ -231,8 +286,8 @@ def equi(im,
     
     """
     
+    # basic quantities
     ht,wd = im.shape[0:2]
-    
     d = focal_length
     r = 12
     
@@ -242,20 +297,23 @@ def equi(im,
     phi_min, lam_min = -phi_max, -lam_max
     Lp_max = d * r * np.sin(phi_max) / (d + r * (np.cos(phi_max) - 1))
     
+    # prepare polar coordinate arrays that span the extent
     phis = np.linspace(phi_min, phi_max, ht, dtype = np.float32)
     lams = np.linspace(lam_min, lam_max, wd, dtype = np.float32)
-    
     phi, lam = np.meshgrid(phis, lams)
 
+    # calculate the source angular coordinates for each pair of destination coordinates
     Lp_x = d * r * np.sin(phi) / (d + r * (np.cos(phi) - 1))
     Lp_y = d * r * np.sin(lam) / (d + r * (np.cos(lam) - 1))
     
     x = np.floor(Lp_x / Lp_max * ht / 2 + ht / 2)
     y = np.floor(Lp_y / Lp_max * wd / 2 + wd / 2)
             
+    # perform the remap
     equi_image = cv2.remap(im, x, y, cv2.INTER_LINEAR, cv2.BORDER_TRANSPARENT) 
             
     return (equi_image, float(lam_max), float(phi_max))
+
 
 def polecap (im, 
             num_gores, 
@@ -274,11 +332,21 @@ def polecap (im,
     
     returns:   output image (PIL.Image)
     """
+    
+    # the function takes an already "swapped" image, so first swap it back to normal
     swapped = swap(im = im, lam_extent = lam_extent, phi_extent = phi_extent)
+    
+    # perform the orthographic projection
     output  = make_equatorial(swapped, num_gores = 1, phi_cap = phi_cap, projection = ORTHOGRAPHIC)
+    
+    # convert to PIL.Image
     polecap = nd2im(output)
+    
+    # rotate the cap to match the orientation of the image produced by make_polar
     polecap = polecap.rotate(- 180 / num_gores)
+    
     return polecap
+
 
 def make_rotary (im, 
                 focal_length, 
@@ -296,14 +364,32 @@ def make_rotary (im,
     projection:      projection to use (constant)
     phi_no_cut:      angle of "no-cut zone" (radians)
     """
+    
+    # create the equirectangular (plate-caree) representation of the fundus
     fundus_equi, lammax, phimax = equi(im = im, 
                           focal_length = focal_length, alpha_max = alpha_max)
+    
+    # rotate the representation so that the centre of the fundus lies at the "north pole"
     fundus_swapped = swap(fundus_equi, phi_extent = phimax, lam_extent = lammax)
+    
+    # get image sizes
     swapped_height, swapped_width = fundus_swapped.shape[:2]
+    
+    # double the width of the image: make_polar expects the equirectangular
+    # representation to be twice as wide as it is high, since the longitude is in
+    # [0,2pi] and latitude is in [-pi/2,pi/2]
     fundus_swapped_resized = cv2.resize(fundus_swapped, (swapped_width * 2, swapped_height)) 
+    
+    # produce the polar gore pattern
     fundus_rotary = make_polar(fundus_swapped_resized, num_gores = num_gores, 
                           projection = projection)
+    
+    # produce the pole cap in the no-cut zone
     fundus_cap = polecap(fundus_swapped_resized, num_gores = num_gores, phi_cap = phi_no_cut)
+    
+    # caculate offsets to ensure that the centre of fundus_cap is over the centre of
+    # fundus_rotary. In each case this is just the distance to move the top/left corner
+    # down and to the right.
     vertical_offset = round((fundus_rotary.height - fundus_cap.height) / 2)
     horizontal_offset = round((fundus_rotary.width - fundus_cap.width) / 2)
     fundus_rotary.paste(fundus_cap, (horizontal_offset, vertical_offset), fundus_cap)
