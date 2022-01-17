8dbdca3a7ca86c54d990a6dbfed7d4031751b7f5
stuwilmur <stuwilmur@users.noreply.github.com>
2022-01-17 21:56:11 +0000

    Add observers to all input widgets

--- gore/ui.py
+++ gore/ui.py
@@ -52,6 +52,7 @@ mypath = "./img"
 imgfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 style = {'description_width': 'initial'}
+layout = widgets.Layout(width='500px', height='40px') #set width and height
 
 w_source_img = widgets.Dropdown(
     options=imgfiles,
@@ -67,10 +68,12 @@ w_focal_length = widgets.FloatSlider(
     step=0.5,
     description='Focal length:',
     disabled=False,
-    continuous_update=False,
+    continuous_update=True,
     orientation='horizontal',
     readout=True,
     readout_format='.1f',
+    style = style,
+    layout = layout,
 )
 display(w_focal_length)
 
@@ -79,8 +82,9 @@ w_alpha_max = widgets.IntSlider(
     max=100,
     value = 32,
     step=1,
-    description='Fundus angle:',
-    style = style)
+    description='Fundus image size (degrees):',
+    style = style,
+    layout = layout)
 display(w_alpha_max)
 
 w_num_gores = widgets.IntSlider(
@@ -88,8 +92,9 @@ w_num_gores = widgets.IntSlider(
     max = 24,
     value = 6,
     step = 1,
-    description = 'Number of gores:',
-    style=style)
+    description = 'Number of gores',
+    style=style,
+    layout = layout)
 display(w_num_gores)
 
 w_alpha_limit = widgets.IntSlider(
@@ -97,7 +102,9 @@ w_alpha_limit = widgets.IntSlider(
     max = 150,
     value = 100,
     step = 5,
-    description = 'Retina angle')
+    description = 'Retinal size (degrees)',
+    style = style,
+    layout = layout)
 display(w_alpha_limit)
 
 w_phi_no_cut = widgets.IntSlider(
@@ -105,8 +112,9 @@ w_phi_no_cut = widgets.IntSlider(
     max = 90,
     value = 10,
     step = 1,
-    description = "Angle of no-cut area",
-    style=style)
+    description = "No-cut area (degrees)",
+    style=style,
+    layout = layout)
 display(w_phi_no_cut)
 
 w_angle = widgets.IntSlider(
@@ -114,8 +122,9 @@ w_angle = widgets.IntSlider(
     max = 360,
     value = 0,
     step = 1,
-    description = "Angle to rotate image",
-    style=style)
+    description = "Rotation (degrees)",
+    style=style,
+    layout = layout)
 display(w_angle)
 
 w_quality = widgets.IntSlider(
@@ -124,7 +133,8 @@ w_quality = widgets.IntSlider(
     value = 20,
     step = 5,
     description = "Quality (%)",
-    style=style)
+    style=style,
+    layout = layout)
 display(w_quality)
 
 w_projection = widgets.Dropdown(
@@ -174,13 +184,20 @@ def calculate(gore_args, allow_save=False):
 def on_calculate(b):
     calculate(get_inputs(), allow_save = True)
 
-@debounce(2)
+@debounce(1)
 def on_edit_parameters(change):
     inputs = get_inputs()
     calculate(inputs)
 
 btn_calculate.on_click(on_calculate)
+w_source_img.observe(on_edit_parameters)
+w_focal_length.observe(on_edit_parameters)
+w_alpha_max.observe(on_edit_parameters)
 w_num_gores.observe(on_edit_parameters)
+w_alpha_limit.observe(on_edit_parameters)
+w_phi_no_cut.observe(on_edit_parameters)
+w_angle.observe(on_edit_parameters)
+w_quality.observe(on_edit_parameters)
 
 display(btn_calculate)
 display(out)
