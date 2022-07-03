9a6e8baa13116d001e29bdad7dc7ff49338f8358
stuwilmur <stuwilmur@users.noreply.github.com>
2022-07-03 15:50:21 +0100

    Change path to splash depending on how app is launched

--- qt/app.py
+++ qt/app.py
@@ -998,7 +998,15 @@ class Worker(QObject):
 
 def main():
     app = QApplication(sys.argv)
-    pixmap = QPixmap("splash.png")
+    if getattr(sys, 'frozen', False):
+    # If the application is run as a bundle, the PyInstaller bootloader
+    # extends the sys module by a flag frozen=True and sets the app 
+    # path into variable _MEIPASS'.
+        application_path = sys._MEIPASS
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__))
+    splash_path = os.path.join(application_path, "splash.png")
+    pixmap = QPixmap(splash_path)
     splash = QSplashScreen(pixmap)
     
     # show the splash screen first...
