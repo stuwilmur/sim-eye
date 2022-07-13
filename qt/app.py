055eca398af9566900aef64c66aa30a509db17be
stuwilmur <stuwilmur@users.noreply.github.com>
2022-07-13 15:36:58 +0100

    Update app.py

--- qt/app.py
+++ qt/app.py
@@ -43,7 +43,7 @@ University of St Andrews 2022
 Credits:
 
 Andrew Blaikie
-Obaid Khousha
+Obaid Kousha
 Stuart Murray
 William J. Williams
 
@@ -90,7 +90,13 @@ class HelpBrowser(QWidget):
         layout.addWidget(self.textEdit)
         self.setLayout(layout)
         
-        dirPath = os.path.dirname(os.path.realpath(__file__))
+        if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app 
+        # path into variable _MEIPASS'.
+            dirPath = os.path.dirname(sys.executable)
+        else:
+            dirPath = os.path.dirname(os.path.abspath(__file__))
         guidePath = os.path.join(dirPath,"userguide.html")
         f = QFile(guidePath)
         f.open(QFile.ReadOnly|QFile.Text)
